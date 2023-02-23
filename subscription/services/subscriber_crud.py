db485ae60184711064a555a6f9aed24d86e26a10
Sumit Chirania <sumitchirania@gmail.com>
2023-02-23 17:57:41 +0530

    serializers used and added along with custome Response + gitignore file

--- subscription/services/subscriber_crud.py
+++ subscription/services/subscriber_crud.py
@@ -10,12 +10,9 @@ class SubscriberCrud(object):
     def __int__(self):
         pass
 
+    def get_subscribers_list(self):
+        return Subscriber.objects.all()
+
     def create_subscriber(self, data):
-        print(data, 'data')
-        success = False
-        try:
-            Subscriber.objects.create(**data)
-            success = True
-        except Exception as e:
-            logger.error(e, exc_info=True)
-        return success
+        return Subscriber.objects.create(**data)
+
--- subscription/views/subscriber.py
+++ subscription/views/subscriber.py
@@ -1,28 +1,31 @@
 import logging
 
 from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from ..services.subscriber_crud import SubscriberCrud
-from django.http import JsonResponse
+from rest_framework.parsers import JSONParser
+from ..serializers.subscribers_serializer import SubSerializer
 
 logger = logging.getLogger(__name__)
 
 
 class SubscriberView(APIView):
-    print(1111112233)
+
+    def get(self, request):
+        subscriber_list = SubscriberCrud().get_subscribers_list()
+        return Response(SubSerializer(subscriber_list, many=True).data, status=status.HTTP_200_OK)
 
     def post(self, request):
-        print(11111)
-        print(request.data)
-        endpoint = request.data['endpoint']
-        auth_key = request.data['auth_key']
-        public_key = request.data['public_key']
-        print(endpoint, 'end', auth_key, 'auth', public_key)
-        subscriber = SubscriberCrud()
-        success = subscriber.create_subscriber(request.data)
-        print(success, 'succ')
-        if success:
-            return JsonResponse({'success': True, 'status': 200, 'msg': 'Subscriber created'})
-        else:
-            return JsonResponse({'success': False, 'status': 500, 'msg': 'Subscriber could not be created'})
 
+        data = JSONParser().parse(request)
+        serializer = SubSerializer(data=data)
+        print(serializer)
+        print(vars(serializer))
+        print(serializer.__dir__())
+        if serializer.is_valid():
+            subscriber = SubscriberCrud().create_subscriber(serializer.validated_data)
+            return Response(SubSerializer(subscriber).data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
